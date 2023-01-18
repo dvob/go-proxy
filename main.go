673e85064375f8b3125f052513f698f85ba66305
David Schneider <dsbrng25b@gmail.com>
2023-01-18 22:02:25 +0100

    Add logging

--- main.go
+++ main.go
@@ -5,10 +5,20 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/http/httputil"
 	"os"
 )
 
+func logRequest(r *http.Request) {
+	requestDump, err := httputil.DumpRequest(r, false)
+	if err != nil {
+		requestDump = []byte("failed to dump request")
+	}
+	log.Printf("scheme=%s host=%s path=%s\n%s", r.URL.Scheme, r.URL.Host, r.URL.Path, requestDump)
+}
+
 func forward(w http.ResponseWriter, r *http.Request) {
+	logRequest(r)
 	resp, err := http.DefaultTransport.RoundTrip(r)
 	if err != nil {
 		log.Print(err)
